550bea25990dfde8d2de36bcb74a75bc8dd33768
Marek Grochowski <grochu@is.umk.pl>
2024-11-12 16:05:02 +0100

    showvars-3 poprawiony

--- showvars-3.sh
+++ showvars-3.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-
 if [ $# -eq 0 ]
 then
    echo "brak argumentu"
    exit 2
 fi
+
+if ! printenv "$1" > /dev/null
+then
+   echo "nieznana zmienna"
+   exit 1
+else
+   echo "$1=$(printenv $1)"
+fi
+
+exit 0
+
+
